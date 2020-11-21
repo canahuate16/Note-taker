7490a321e026ebd7e9d3f326f63accdf51ddbc43
Luis Canahuate <luis.canahuate16@gmail.com>
2020-11-21 13:36:13 -0500

    updated pk.json file with server.js name and changed html route to /

--- server.js
+++ server.js
@@ -68,6 +68,9 @@ app.get ('/api/notes/:id', function (req,res){
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
